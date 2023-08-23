da5826d11b592c9db9cb0a7cc2cff24f17847778
Abhayiith <ms21btech11002@iith.ac.in>
2023-08-23 13:03:46 +0530

    file structure updated

---
+++ huffmanAlgo/src/huff/App.java
@@ -0,0 +1,17 @@
+package huff;
+
+public class App {
+    public static void main(String[] args) {
+
+        Huffman huffman = new Huffman("aaaaaaaabbbbbbbccccdd");
+
+        String encodedText = huffman.encode();
+        System.out.println(encodedText);
+
+        huffman.printCodes();
+
+        String originalText = huffman.decode(encodedText);
+        System.out.println(originalText);
+
+    }
+}
---
+++ huffmanAlgo/src/huff/Leaf.java
@@ -0,0 +1,17 @@
+package huff;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class Leaf extends Node {
+
+    private final char character;
+
+    public Leaf(char character, int frequency) {
+        super(frequency);
+        this.character = character;
+    }
+
+}
